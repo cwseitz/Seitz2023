06070afe7e77d38b67f326dd2622bd3abec1089d
cwseitz <cwseitz2@gmail.com>
2022-12-09 16:04:23 -0500

    udpate

--- mkdirs.py
+++
@@ -1,7 +0,0 @@
-import os
-from pathlib import Path
-path = '/home/cwseitz/mnt/gpu/Data/'
-path2 = '/home/cwseitz/mnt/gpu/Analysis/'
-dirs = next(os.walk(path))[1]
-for dir in dirs:
-    Path(path2+dir).mkdir(parents=True, exist_ok=True)
