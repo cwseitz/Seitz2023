91b3493c20d4a9fc2a9d24efa8bdda549c2a72c5
cwseitz <cwseitz2@gmail.com>
2022-12-11 20:38:01 -0500

    update

--- main.py
+++ main.py
@@ -3,27 +3,32 @@ from correct import Basic
 from manual import Analyzer
 from pathlib import Path
 
+path0 = '/home/cwseitz/mnt/gpu/Data/'
+
 paths = [
-'/home/cwseitz/mnt/gpu/Data/221206-Hela-IFNG-1h-1_1/',
-#'/home/cwseitz/mnt/gpu/Data/221206-Hela-IFNG-1h-2_1/',
-#'/home/cwseitz/mnt/gpu/Data/221206-Hela-IFNG-1h-3_2/',
-#'/home/cwseitz/mnt/gpu/Data/221206-Hela-IFNG-1h-4_1/',
-#'/home/cwseitz/mnt/gpu/Data/221130-Hela-IFNG-2h-1_1/',
-#'/home/cwseitz/mnt/gpu/Data/221130-Hela-IFNG-4h-3_1/',
-#'/home/cwseitz/mnt/gpu/Data/221130-Hela-IFNG-4h-3_2/',
-#'/home/cwseitz/mnt/gpu/Data/221130-Hela-IFNG-4h-4_1/',
-#'/home/cwseitz/mnt/gpu/Data/221130-Hela-IFNG-8h-1_1/',
-#'/home/cwseitz/mnt/gpu/Data/221130-Hela-IFNG-8h-2_1/',
-#'/home/cwseitz/mnt/gpu/Data/221130-Hela-IFNG-8h-3_1/',
-#'/home/cwseitz/mnt/gpu/Data/221130-Hela-IFNG-8h-4_1/'
-#'/home/cwseitz/mnt/gpu/Data/221206-Hela-IFNG-16h-1_1/',
-#'/home/cwseitz/mnt/gpu/Data/221206-Hela-IFNG-16h-2_1/',
-#'/home/cwseitz/mnt/gpu/Data/221206-Hela-IFNG-16h-3_1/',
-#'/home/cwseitz/mnt/gpu/Data/221206-Hela-IFNG-16h-4_1/',
-#'/home/cwseitz/mnt/gpu/Data/221206-Hela-IFNG-24h-1_1/',
-#'/home/cwseitz/mnt/gpu/Data/221206-Hela-IFNG-24h-3_1/',
-#'/home/cwseitz/mnt/gpu/Data/221206-Hela-IFNG-24h-4_1/'
+#'221206-Hela-IFNG-1h-1_1/', #GAPDH channel is not very good
+#'221206-Hela-IFNG-1h-2_1/',
+#'221206-Hela-IFNG-1h-3_2/',
+#'221206-Hela-IFNG-1h-4_1/',
+'221130-Hela-IFNG-2h-1_1/',
+#'221130-Hela-IFNG-4h-3_1/',
+#'221130-Hela-IFNG-4h-3_2/',
+#'221130-Hela-IFNG-4h-4_1/',
+#'221130-Hela-IFNG-8h-1_1/',
+#'221130-Hela-IFNG-8h-2_1/',
+#'221130-Hela-IFNG-8h-3_1/',
+#'221130-Hela-IFNG-8h-4_1/'
+#'221206-Hela-IFNG-16h-1_1/',
+#'221206-Hela-IFNG-16h-2_1/',
+#'221206-Hela-IFNG-16h-3_1/',
+#'221206-Hela-IFNG-16h-4_1/',
+#'221206-Hela-IFNG-24h-1_1/',
+#'221206-Hela-IFNG-24h-3_1/',
+#'221206-Hela-IFNG-24h-4_1/'
 ]
+
+for i,path in enumerate(paths): 
+    paths[i] = path0+paths[i]
 
 def get_opath(ipath):
     return ipath.replace('Data', 'Analysis')
@@ -35,10 +40,10 @@ for ipath in paths:
     print("Processing " + prefix)
     opath = get_opath(ipath)
     Path(opath).mkdir(parents=True, exist_ok=True)
-    tiler = Tiler(ipath,opath,prefix)
-    tiler.tile()
-    basic = Basic(opath,prefix)
-    basic.correct()
-    #analyzer = Analyzer(prefix,opath)
-    #analyzer.analyze(filters)
+    #tiler = Tiler(ipath,opath,prefix)
+    #tiler.tile()
+    #basic = Basic(opath,prefix)
+    #basic.correct()
+    analyzer = Analyzer(prefix,opath)
+    analyzer.analyze()
 
--- manual.py
+++ manual.py
@@ -4,8 +4,11 @@ import tifffile
 import math
 import pandas as pd
 from skimage.segmentation import mark_boundaries
+from skimage.restoration import rolling_ball
+from skimage.filters import gaussian
+from skimage.util import img_as_ubyte, img_as_uint
 from smlm.track import LOGDetector
-from smlm.filters import blur
+from smlm.filters import blur, boxcar
 from smlm.plot import anno_blob
 import napari 
 
@@ -21,53 +24,65 @@ class Analyzer:
         rgb = np.dstack((ch2/ch2_max,ch1/ch1_max,ch0/ch0_max))
         return rgb
     def plot_result(self,df1,df2,ch0,ch1,ch2,labels,n):
-        fig, ax = plt.subplots(figsize=(10,10))   
+        fig, ax = plt.subplots(figsize=(10,10)) 
         rgb = self.get_rgb(ch0,ch1,ch2)
         rgb = mark_boundaries(rgb,labels,color=(0.8,0.8,0.8))
         ax.imshow(rgb)
-        ax.scatter(df1['y'],df1['x'],marker='x',s=5,color='yellow',label='GAPDH')
-        ax.scatter(df2['y'],df2['x'],marker='x',s=5,color='purple',label='GBP5')
+        anno_blob(ax, df1, color='yellow')
+        anno_blob(ax, df2, color='red')
         ax.set_xticks([])
         ax.set_yticks([])
         ax.legend()
         plt.tight_layout()
-        plt.savefig(self.opath + self.pfx + self.sfx + f'{n}.png')
-    def analyze(self,filters):
+        plt.show()
+        #plt.savefig(self.opath + self.pfx + self.sfx + f'{n}.png')
+    def filter(self,image):
+        blurred = gaussian(image,sigma=1)
+        blurred = blurred/blurred.max()
+        blurred = img_as_uint(blurred)
+        bg = rolling_ball(blurred,radius=5)    
+        return blurred-bg  
+    def analyze(self,n0=0):
         print("Loading dataset...")
         ch0_stack = tifffile.imread(self.opath + self.pfx + self.sfx + 'ch0.tif')
         ch1_stack = tifffile.imread(self.opath + self.pfx + self.sfx + 'ch1.tif')
         ch2_stack = tifffile.imread(self.opath + self.pfx + self.sfx + 'ch2.tif')
         print("Done.")
         nt,nx,ny = ch0_stack.shape
-        for n in range(nt):
+        for n in range(n0,nt):
             flag = True
             while flag:
                 viewer = napari.Viewer()
                 viewer.window.resize(2000, 1000)
                 viewer.add_image(ch0_stack[n],name='DAPI',colormap='blue',visible=False)
-                viewer.add_image(blur(ch1_stack[n],sigma=1),name='GAPDH',colormap='green')
-                viewer.add_image(blur(ch2_stack[n],sigma=1),name='GBP5',colormap='red',visible=False)
+                viewer.add_image(self.filter(ch1_stack[n]),name='GAPDH',colormap='green')
+                viewer.add_image(self.filter(ch2_stack[n]),name='GBP5',colormap='red',visible=False)
                 napari.run()
                 shape = ch1_stack[n].shape
-                try:
-                    ch1_pts = viewer.layers['GAPDH-features-blob_log'].data
-                    ch1_pts = ch1_pts.astype(np.uint16)
-                    ch2_pts = viewer.layers['GBP5-features-blob_log'].data
-                    ch2_pts = ch2_pts.astype(np.uint16)
-                    labels = viewer.layers['Shapes'].to_labels(labels_shape=shape)
-                    ch1_pts_labels = np.expand_dims(labels[ch1_pts[:,0],ch1_pts[:,1]],axis=1)
-                    ch1_pts = np.concatenate((ch1_pts,ch1_pts_labels),axis=1)
-                    df1 = pd.DataFrame({'x':ch1_pts[:, 0],'y':ch1_pts[:, 1],'label':ch1_pts[:,2]})
-                    df1.to_csv(self.opath + self.pfx + self.sfx + f'gapdh_{n}.csv')
-                    ch2_pts_labels = np.expand_dims(labels[ch2_pts[:,0],ch2_pts[:,1]],axis=1)
-                    ch2_pts = np.concatenate((ch2_pts,ch2_pts_labels),axis=1)
-                    df2 = pd.DataFrame({'x':ch2_pts[:, 0],'y':ch2_pts[:, 1],'label':ch2_pts[:,2]})
-                    df2.to_csv(self.opath + self.pfx + self.sfx + f'gbp5_{n}.csv')
-                    self.plot_result(df1,df2,ch0_stack[n],ch1_stack[n],ch2_stack[n],labels,n)
+                ans = input("Save? (y/n): ")
+                if ans == 'y':
+                    try:
+                        ch1_pts = viewer.layers['GAPDH-features-blob_log'].data
+                        ch1_pts = ch1_pts.astype(np.uint16)
+                        ch2_pts = viewer.layers['GBP5-features-blob_log'].data
+                        ch2_pts = ch2_pts.astype(np.uint16)
+                        labels = viewer.layers['Shapes'].to_labels(labels_shape=shape)
+                        ch1_pts_labels = np.expand_dims(labels[ch1_pts[:,0],ch1_pts[:,1]],axis=1)
+                        ch1_pts = np.concatenate((ch1_pts,ch1_pts_labels),axis=1)
+                        df1 = pd.DataFrame({'x':ch1_pts[:, 0],'y':ch1_pts[:, 1],'label':ch1_pts[:,2]})
+                        df1.to_csv(self.opath + self.pfx + self.sfx + f'gapdh_{n}.csv')
+                        ch2_pts_labels = np.expand_dims(labels[ch2_pts[:,0],ch2_pts[:,1]],axis=1)
+                        ch2_pts = np.concatenate((ch2_pts,ch2_pts_labels),axis=1)
+                        df2 = pd.DataFrame({'x':ch2_pts[:, 0],'y':ch2_pts[:, 1],'label':ch2_pts[:,2]})
+                        df2.to_csv(self.opath + self.pfx + self.sfx + f'gbp5_{n}.csv')
+                        self.plot_result(df1,df2,ch0_stack[n],
+                                         self.filter(ch1_stack[n]),self.filter(ch2_stack[n]),labels,n)
+                        flag = False
+                    except Exception as e:
+                        print(e)
+                        print('ERROR: Missing layer: Restarting...')
+                else:
                     flag = False
-                except Exception as e:
-                    print(e)
-                    print('ERROR: Missing layer: Restarting...')
 
         
 
