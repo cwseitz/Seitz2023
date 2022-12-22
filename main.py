befddc271969abda4bc1b9d2df9ddbd9df5b9f0a
clayton <cwseitz@iu.edu>
2022-12-21 21:29:19 -0500

    update

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ from manual import Analyzer
 from pathlib import Path
 from check import Checker
 
-path0 = '/home/cwseitz/mnt/gpu/Data/'
+path0 = '/research3/shared/cwseitz/Data/'
 
 paths = [
 #'221206-Hela-IFNG-1h-1_1/', #GAPDH channel is not very good
 #'221206-Hela-IFNG-1h-2_1/',
 #'221206-Hela-IFNG-1h-3_2/',
 #'221206-Hela-IFNG-1h-4_1/',
-'221130-Hela-IFNG-2h-1_1/',
+#'221130-Hela-IFNG-2h-1_1/',
 #'221130-Hela-IFNG-4h-3_1/',
 #'221130-Hela-IFNG-4h-3_2/',
 #'221130-Hela-IFNG-4h-4_1/',
@@ -26,6 +26,8 @@ paths = [
 #'221206-Hela-IFNG-24h-1_1/',
 #'221206-Hela-IFNG-24h-3_1/',
 #'221206-Hela-IFNG-24h-4_1/'
+#'221218-Hela-IFNG-16h-2_1/'
+'221218-Hela-IFNG-4h-2_1/'
 ]
 
 for i,path in enumerate(paths): 
@@ -41,12 +43,12 @@ for ipath in paths:
     print("Processing " + prefix)
     opath = get_opath(ipath)
     Path(opath).mkdir(parents=True, exist_ok=True)
-    #tiler = Tiler(ipath,opath,prefix)
-    #tiler.tile()
-    #basic = Basic(opath,prefix)
-    #basic.correct()
+    tiler = Tiler(ipath,opath,prefix)
+    tiler.tile()
+    basic = Basic(opath,prefix)
+    basic.correct()
     #analyzer = Analyzer(prefix,opath)
-    #analyzer.analyze(n0=0)
-    checker = Checker(prefix,opath)
-    checker.check()
+    #analyzer.analyze(n0=7)
+    #checker = Checker(prefix,opath)
+    #checker.check()
 
